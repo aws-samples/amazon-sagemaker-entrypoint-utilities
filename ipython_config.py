59e963e4189dc54fcdbffd08c3cff36dfb693c08
Verdi March <marcverd@amazon.com>
2020-03-09 17:10:09 +0800

    ipython_config.py: support Windows paths

--- ipython_config.py
+++ ipython_config.py
@@ -1,14 +1,19 @@
 import os
+from pathlib import Path
 import subprocess
 
 ####################################################################################################
 # Additional PYTHONPATH to allow notebooks to import custom modules at a few pre-defined places.
 
-_cwd = os.getcwd()
-_line = 'sys.path.append("{}")'
+
+def sys_path_append(o: os.PathLike) -> str:
+    posix_path: str = o.as_posix() if isinstance(o, Path) else Path(o).as_posix()
+    return 'sys.path.append("{}")'.format(posix_path)
+
+
 _pythonpath = [
     "import sys, os",
-    _line.format(os.getcwd()),
+    sys_path_append(os.getcwd()),
 ]
 
 # Add GIT_ROOT/ and a few other subdirs
@@ -16,11 +21,12 @@ try:
     _p = subprocess.run(["git", "rev-parse", "--show-toplevel"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
     if _p.returncode == 0:
-        _git_root = _p.stdout[:-1].decode("utf-8")  # Remove trailing '\n'
+        _git_root: str = _p.stdout[:-1].decode("utf-8")  # Remove trailing '\n'
+        _git_root_p: Path = Path(_git_root)
         _pythonpath += [
-            _line.format(_git_root),  # GIT_ROOT
-            _line.format(os.path.join(_git_root, "src")),  # GIT_ROOT/src
-            _line.format(os.path.join(_git_root, "notebooks")),  # GIT_ROOT/notebooks
+            sys_path_append(_git_root_p),  # GIT_ROOT
+            sys_path_append(_git_root_p / "src"),  # GIT_ROOT/src
+            sys_path_append(_git_root_p / "notebooks"),  # GIT_ROOT/notebooks
         ]
 except:  # noqa: E722
     pass
